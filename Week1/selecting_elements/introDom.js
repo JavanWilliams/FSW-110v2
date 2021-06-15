aa440df99d05db827b819d0fc424cc4d0782171e
JavanWilliams <javan.williams@bryanuniversity.edu>
2021-06-15 05:27:19 -0700

    working

---
+++ Week1/selecting_elements/introDom.js
@@ -0,0 +1,1 @@
+var first = document.getElementById("hed1").innerHTML = "MMA";
---
+++ Week1/step_6/index.js
@@ -0,0 +1,3 @@
+document.getElementById("one").textContent =  "Did you know Rose Namajunas is the UFC Straweight Champion? Now you do";
+
+
